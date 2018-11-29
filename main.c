932920a33cb94a5b695c26b8db49846870b150bd
Stefan Tan <stan3@stuy.edu>
2018-11-28 22:18:41 -0500

    fixed some of main.c

--- main.c
+++ main.c
@@ -4,8 +4,14 @@
 #include <sys/wait.h>
 #include <string.h>
 
-char ** parse_args(int nargs,char * line ) {
-  char ** a = malloc(nargs * sizeof(char *));
+void printCurDir() { 
+    char cwd[256]; 
+    getcwd(cwd, sizeof(cwd)); 
+    printf("\ncwd: %s$ ", cwd); 
+} 
+
+char ** parse_args( char * line ) {
+  char ** a = malloc(100 * sizeof(char *));
   int i = 0;
   while(line) {
     a[i] = strsep(&line, " ");
@@ -14,36 +20,65 @@ char ** parse_args(int nargs,char * line ) {
   return a;
 }
 
-int main(){
-  int status, f, cpid, nargs;
-  char *s;
-  fgets(s, 100, stdin);
-  for(int i = 0; s[i]; i++){
-    if(!(strcmp(s[i], " ")))
-      nargs++;
+int exec(char ** args){
+  int f, status;
+
+  if (!strcmp(args[0], "cd")){
+    chdir(args[1]);
   }
-  
-  char ** args = parse_args(nargs, s);
-  
+  /* DOESNT WORK
+  if (!strcmp(args[0], "exit")){
+    return 0;
+  }
+  */
   f = fork();
   
   if (!f){
-    printf("Child executing\n");
-    execvp(args[1], args);
-    printf("Error executing\n");
-  }else if(f > 0){
+    execvp(args[0], args);
+  }
+  else {
+    wait(&status);
+    return 0;
+  }
+} 
+  
+
+int main(){
+  int status, f;
+  
+  while(1){
+    //printCurDir(); DOESNT WORK IT KEEPS PRINTING THIS
+    char *s = malloc(100 * sizeof(char));
+    scanf("%[^\n]s", s);
+    
+    char ** args = parse_args(s);
+    exec(args);
+    /* 
+       f = fork();
+    
+       if (!f){
+       // printf("Child executing\n");
+       execvp(args[0], args);
+       return 0;
+       //printf("Error executing\n");
+       }
+       else {
+       wait(&status);
+       }*/
+  }
+  /*
+    else if(f > 0){
 
     if((cpid = wait(&status)) < 0){
-      printf("Waiting");
-      exit(1);
+    printf("Waiting");
+    exit(1);
     }
-
+    
     printf("Parent: Complete\n");
-  }else{
+    }else{
     printf("Fork failed\n");
     exit(1);
-  }
-  printf("%s \n",args[2]);
-    return 0;
+    }*/
+  return 0;
 
 }
