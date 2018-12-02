7e9e392de0beeeb48fbae711b8d44d4adb45159a
Stefan Tan <stan3@stuy.edu>
2018-12-02 01:06:02 -0500

    finished basic shell

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
 
 #include "main.h"
 
@@ -10,22 +11,71 @@ void printCurDir() {
   char cwd[256]; 
   getcwd(cwd, sizeof(cwd)); 
   printf("\ncwd: %s$ ", cwd); 
-} 
+}
 
-/*char ** parse_args( char * line ) {
-  char ** a = malloc(100 * sizeof(char *));
-  int i = 0;
-  while(line) {
-  a[i] = strsep(&line, " ");
-  i++;
+void redirectOut(char ** args, int nargs){
+  int f, status;
+  f = fork(); 
+  if (!f){
+    int fd = open(args[nargs-1], O_WRONLY | O_CREAT, 0777);
+    dup2(fd, 1);
+    args[nargs-2] = NULL;
+    execvp(args[0], args);
   }
-  return a;
+  else {
+    wait(&status);
   }
-*/
+}
+
+void redirectIn(char ** args, int nargs){
+  int f, status;
+  f = fork(); 
+  if (!f){
+    int fd = open(args[nargs-1], O_RDONLY);
+    char s[1024];
+    char cur[256];
+    dup2(fd, 0);
+    while( fgets(cur, 256, stdin))
+      strcat(s, cur);
+    args[nargs - 2] = s;
+    args[nargs - 1] = NULL;
+    execvp(args[0], args);
+  }
+  else {
+    wait(&status);
+  }
+}
+
+void redirectOutApp(char ** args, int nargs){
+  int f, status;
+  f = fork(); 
+  if (!f){
+    int fd = open(args[nargs-1], O_WRONLY | O_CREAT | O_APPEND, 0777);
+    dup2(fd, 1);
+    args[nargs-2] = NULL;
+    execvp(args[0], args);
+  }
+  else {
+    wait(&status);
+  }
+}
+
+int piper(char ** args){
+  char readbuf[100];
+  FILE *pipein, *pipeout;
+
+  pipein = popen(args[0], "r");
+  pipeout = popen(args[2], "w");
+    
+  while (fgets(readbuf, 100, pipein))
+    fputs(readbuf, pipeout);
+  pclose(pipein);
+  pclose(pipeout);
+  return 0;
+}
 
 void exec(char ** args){
-  int f, status;
-
+  int f, status;  
   if (!strcmp(args[0], "cd")){
     chdir(args[1]);
   }
@@ -40,7 +90,6 @@ void exec(char ** args){
     wait(&status);
   }
 } 
-  
 
 int main(){
   char line[50];
@@ -70,7 +119,19 @@ int main(){
 	tok = strtok(NULL," ");
       }
       args[argcount] = NULL;
-      if(argcount > 0){
+      if (! strcmp(args[argcount-2], ">" ) ){
+	redirectOut(args, argcount);
+      }
+      if (! strcmp(args[argcount-2], "<") ){
+	redirectIn(args, argcount);
+      }
+      if (! strcmp(args[argcount-2], ">>" ) ){
+	redirectOutApp(args, argcount);
+      }
+      if (! strcmp(args[argcount-2], "|" ) ){
+	piper(args);
+      }
+      else if(argcount > 0){
 	exec(args);
       }
       i++;
--- main.h
+++ main.h
@@ -1,3 +1,9 @@
 void printCurDir();
 
+void redirectout(char ** args, int nargs);
+
+void redirectin(char ** args, int nargs);
+
+void my_append(char ** args, int nargs);
+
 void exec(char ** args);
