70043f718b3c87bf5b4a2c88d5bd218fd35e8a1d
Stefan Tan <stan3@stuy.edu>
2018-12-01 20:08:22 -0500

    multiple commands now work

--- main.c
+++ main.c
@@ -5,22 +5,22 @@
 #include <string.h>
 
 void printCurDir() { 
-    char cwd[256]; 
-    getcwd(cwd, sizeof(cwd)); 
-    printf("\ncwd: %s$ ", cwd); 
+  char cwd[256]; 
+  getcwd(cwd, sizeof(cwd)); 
+  printf("\ncwd: %s$ ", cwd); 
 } 
 
 /*char ** parse_args( char * line ) {
   char ** a = malloc(100 * sizeof(char *));
   int i = 0;
   while(line) {
-    a[i] = strsep(&line, " ");
-    i++;
+  a[i] = strsep(&line, " ");
+  i++;
   }
   return a;
-}
-
+  }
 */
+
 void exec(char ** args){
   int f, status;
 
@@ -46,18 +46,32 @@ int main(){
   while(fgets(line,50, stdin)){
     line[strlen(line)-1] = '\0';
     char *args[50];
-    int argcount = 0;
-
-    char* tok = strtok(line, " ");
-    
-    while(tok){ //if(tok == ";")
-      args[argcount] = tok;
-      argcount++;
-      tok = strtok(NULL," ");
+    char * token;
+    int numcmd = 1;
+    char ** cmds = malloc(100 * sizeof(char *));
+    token = strtok(line, ";");
+    while(token) {
+      cmds[numcmd-1] = token;
+      token = strtok(NULL, ";");
+      numcmd++;
     }
-    args[argcount] = NULL;
-    if(argcount > 0){
-      exec(args);
+
+    int i = 0;
+    while(numcmd > 1){
+      int argcount = 0;
+      char* tok = strtok(cmds[i], " ");
+    
+      while(tok){
+	args[argcount] = tok;
+	argcount++;
+	tok = strtok(NULL," ");
+      }
+      args[argcount] = NULL;
+      if(argcount > 0){
+	exec(args);
+      }
+      i++;
+      numcmd--;
     }
     printCurDir();
   }
